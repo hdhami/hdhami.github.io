3f6d88b8b46f272c0b62cff5687d56e6339db0ab
hdhami <hsd.1989@gmail.com>
2020-01-31 15:10:54 +0530

    added js for pagination

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import reactDom from 'react-dom';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
     input: 'src/pagination.js',
@@ -22,6 +23,7 @@ export default {
                 react: Object.keys(react),
                 'react-dom': Object.keys(reactDom)
             }
-        })
+        }),
+        terser()
     ]
 };
