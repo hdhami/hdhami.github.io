6c431e99659887a881f7a7b2fa0bb0afbc1ec177
hdhami <hsd.1989@gmail.com>
2020-02-01 18:36:09 +0530

    bump react pagination

--- src/client/app/components/root/app.js
+++ src/client/app/components/root/app.js
@@ -14,6 +14,19 @@ const PaginationDemo = () => {
                 onPaginate={pageId => {
                     setActivePage(pageId);
                 }}
+                styles={{
+                    navigationBtns: {
+                        activeIconColor: '#000',
+                        inactiveIconColor: '#b5b5b5',
+                        borderColor: '#eeeeee'
+                    },
+                    paginationCells: {
+                        padding: '0 12px',
+                        activeBgColor: '#92a772',
+                        inactiveBgColor: '#f2f2f2',
+                        textColor: '#000000'
+                    }
+                }}
             />
         </div>
     );
