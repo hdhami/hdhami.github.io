e86bd655dcae3096b24aee04485d058691d40be0
hdhami <hsd.1989@gmail.com>
2020-02-01 15:04:49 +0530

    display flex container

--- src/client/app/components/root/app.js
+++ src/client/app/components/root/app.js
@@ -5,15 +5,17 @@ const PaginationDemo = () => {
     const [activePage, setActivePage] = useState(1);
 
     return (
-        <Pagination
-            range={3}
-            activePage={activePage}
-            totalCount={27}
-            itemsPerPage={5}
-            onPaginate={pageId => {
-                setActivePage(pageId);
-            }}
-        />
+        <div className="pagination-container">
+            <Pagination
+                range={3}
+                activePage={activePage}
+                totalCount={27}
+                itemsPerPage={5}
+                onPaginate={pageId => {
+                    setActivePage(pageId);
+                }}
+            />
+        </div>
     );
 };
 export default PaginationDemo;
